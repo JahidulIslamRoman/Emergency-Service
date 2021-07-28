9c9b8e19cc788a1a72d7252c52a2881baab99139
Jahidul Islam <jahidul.islam02@northsouth.edu>
2021-07-28 22:29:45 +0600

    first commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace EmergencyService
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.lnkLogOut = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Location = new System.Drawing.Point(138, 180);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(116, 13);
+            this.lblWelcome.TabIndex = 0;
+            this.lblWelcome.Text = "Welcome to the Home ";
+            // 
+            // lnkLogOut
+            // 
+            this.lnkLogOut.AutoSize = true;
+            this.lnkLogOut.Location = new System.Drawing.Point(332, 54);
+            this.lnkLogOut.Name = "lnkLogOut";
+            this.lnkLogOut.Size = new System.Drawing.Size(45, 13);
+            this.lnkLogOut.TabIndex = 1;
+            this.lnkLogOut.TabStop = true;
+            this.lnkLogOut.Text = "Log Out";
+            this.lnkLogOut.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkLogOut_LinkClicked);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 450);
+            this.Controls.Add(this.lnkLogOut);
+            this.Controls.Add(this.lblWelcome);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.LinkLabel lnkLogOut;
+    }
+}
